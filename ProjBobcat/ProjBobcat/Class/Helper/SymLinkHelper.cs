33e09317cf9dc186a9228b7d9cd5eaf3811d2a16
Feiron <53085628+fr1g@users.noreply.github.com>
2023-03-24 01:02:49 +0800

    backup

--- ProjBobcat/ProjBobcat/Class/Helper/SymLinkHelper.cs
+++ ProjBobcat/ProjBobcat/Class/Helper/SymLinkHelper.cs
@@ -18,7 +18,7 @@ namespace ProjBobcat.Class.Helper
             ///	需要创建链接到具体版本的资源文件夹的单个文件之路径.(需要包含文件名与后缀)
             /// </param>
             /// <param name="OverrideDefaultLocation">
-            ///	链接需要被创建到的路径(需要包含文件名与后缀), 可为null
+            ///	链接需要被创建到的路径(需要包含文件名与后缀), 可为null, 默认值在创建对象时候创建(强烈建议传一个具体的)
             /// </param>
             /// <param name="ParseToWindowsPath">
             /// 可null的值. 为true时将把两个路径中的/转换为\
@@ -29,17 +29,19 @@ namespace ProjBobcat.Class.Helper
 
 			if (OverrideDefaultLocation is not null) ThisDefaultLocation = OverrideDefaultLocation;
 			else ThisDefaultLocation = _defaultPublicResourceLocation;
+			// 未验证: 是否可以在不给文件后缀的情况下创建到此目录的默认同名文件... 若不能, 将在后续添加文件名检测和自动拼接.
 
 			if(ParseToWindowsPath is not null) if ((bool)ParseToWindowsPath)
 			{
 					ThisDefaultLocation = ThisDefaultLocation.Replace("/", "\\");
 					TargetItem = TargetItem.Replace("/", "\\");
 			}
-			try {
-				string? CmdReturn = "";
+            string? CmdReturn = "";
+            try {
+				
 #if WINDOWS
 		// 原来从vista就自带mklink了哇 那就不考虑junction工具了.
-			CmdReturn = CommandLineHelper.RunCMD("mklink", $"/h {TargetItem} {ThisDefaultLocation}");
+			CmdReturn = CommandLineHelper.RunCMD("mklink", $"/d {TargetItem} {ThisDefaultLocation}");
 			//? 待验证: /d与/h是否: 跨分区, 增加占用, 可被Minecraft识别
 #elif OSX
             CmdReturn = CommandLineHelper.RunBash($"ln -s {TargetItem} {ThisDefaultLocation}");
@@ -55,7 +57,36 @@ namespace ProjBobcat.Class.Helper
 				return false;
 			}
 
+			if (CmdReturn.Contains("nvalid")) return false; // 收集可能的错误信息. 但是返回信息可能是不同的语言, 考虑移除...
 
+            return true;
+		}
+
+		public bool RemoveLink(string TargetLink)
+		{
+            string? CmdReturn = "";
+
+            try
+            {
+
+#if WINDOWS
+		// 原来从vista就自带mklink了哇 那就不考虑junction工具了.
+			CmdReturn = CommandLineHelper.RunCMD("erase", $"/Q {TargetLink}");
+			//? 待验证: /d与/h是否: 跨分区, 增加占用, 可被Minecraft识别
+#elif OSX
+                CmdReturn = CommandLineHelper.RunBash($"rm -rf {TargetLink}");
+                // 一般而言, mac与linux用户可以不考虑跨分区问题. 可以在确认软链接可以被minecraft识别后直接全部软链接
+#elif LINUX
+			CmdReturn = CommandLineHelper.RunBash($"unlink {TargetLink}");
+			// 这应该||就好了吧……不确定
+#endif
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            if (CmdReturn.Contains("nvalid")) return false; // 同上...
             return true;
 		}
 
